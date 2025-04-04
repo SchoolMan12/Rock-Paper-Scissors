ffd2f9cd08c98f4884971db75bbd987887ae5baf
Logan Gifford <802454@sylvaniastudents.org>
2025-04-04 17:19:01 -0400

    Completed rock paper scissors assignment

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,10 +1,30 @@
 function roll(choice) {
-  //var guessingnumber=Math.floor(Math.random() * 10) + 1;
-  //let myinput=document.getElementById("fname");
-  let messagecenter=document.getElementById("messagecenter");
-  //if (myinput.value==guessingnumber) {
-    messagecenter.innerHTML="Congratulations! You guessed the correct number." + choice; 
-  //} else {
-    //messagecenter.innerHTML="Sorry, the correct number was " + guessingnumber +". Better luck next time!";
-  //}
+  //Created an array to hold the rock paper scissors choices
+  var choices = ['🗿', '🧻', '✂️'];
+  //This picks a random number 1-3
+  var guessingnumber = Math.floor(Math.random() * 3);
+  //This makes a variable, computer and sets it equal 
+  var computer = choices[guessingnumber];
+  //This gets the html element to write out what the person chose and computer chose
+  let messagecenter = document.getElementById("messagecenter");
+  messagecenter.innerHTML = "Your choice: " + choice + " Computers choice: " + computer;
+  let results = document.getElementById("results");
+
+  //A tie, if user choice matchs computer choice, they tie  
+  if (choice === computer) {
+    results.innerHTML = "You Tie! ";
+  //If the choice is rock and the computer is scissors, the user wins
+  } else if (choice === '🗿' && '✂️' === computer) {
+    results.innerHTML = "You Win! ";
+  //If the choice is paper and the computer is rock, the user wins
+  } else if (choice === '🧻' && '🗿' === computer) {
+    results.innerHTML = "You Win! ";
+  //If the choice is scissors and the computer is paper, the user wins
+  } else if (choice === '✂️' && '🧻' === computer) {
+    results.innerHTML = "You Win! ";
+  //All other cases, the user loses
+  } else {
+    results.innerHTML = "You Lose";
+  }
+
 }
